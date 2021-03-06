04cbd46fc3f8fb0b72854b168379166067e3176f
Masharelli <a01633090@itesm.mx>
2021-03-06 16:33:04 -0600

    Suport sub-document Mongoose

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -3,31 +3,29 @@ const mongoose = require('mongoose');
 const Dishes = require('./models/dishes');
 
 const url = 'mongodb://localhost:27017/conFusion';
-const connect = mongoose.connect(url);
+const connect = mongoose.connect(url,  {useNewUrlParser: true});
 
 
 connect.then((db)=>{
     console.log("Connected correctly to server")
 
-    var newDish = Dishes({
+    Dishes.create({
         name: 'Uthappizza',
         description: 'test'
-    });
+    }) 
+    .then((dish)=>{
+        console.log(dish);
 
-    newDish.save()
-        .then((dish)=>{
-            console.log(dish);
-
-            return Dishes.find({}).exec();
-        })
-        .then((dishes)=>{
-            console.log(dishes);
-            return Dishes.deleteMany({});
-        })
-        .then(()=>{
-            return mongoose.connection.close();
-        })
-        .catch((err)=>{
-            console.log(err );
-        })
+        return Dishes.find({}).exec();
+    })
+    .then((dishes)=>{
+        console.log(dishes);
+        return Dishes.deleteMany({});
+    })
+    .then(()=>{
+        return mongoose.connection.close();
+    })
+    .catch((err)=>{
+        console.log(err );
+    })
 });
