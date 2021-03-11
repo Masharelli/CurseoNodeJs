fdbf785503f573779cb6673ea12bc0013ac190f1
Hugo Masharelli <a01633090@itesm.mx>
2021-03-10 18:17:11 -0600

    express session module

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -38,42 +38,27 @@ app.use(express.urlencoded({ extended: false }));
    secret: '12345-67890-09876-54321',
    resave: false,
    store: new FileStore
- }))
+ }));
 
-function auth(req, res, next){
-  console.log(req.signedCookies);
+ //Los ponemos antes de authenticar para poder authenticarlos
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
-  if(!req.session.user){
-    var authHeader = req.headers.authorization;
+function auth (req, res, next) {
+    console.log(req.session);
 
-    if(!authHeader){
-      var err = new Error('You are not authenticated! ');
-      res.setHeader('www-Authenticate', 'Basic');
-      err.status = 401;
+  if(!req.session.user) {
+      var err = new Error('You are not authenticated!');
+      err.status = 403;
       return next(err);
-    }
-  
-    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-  
-    var username = auth[0];
-    var password = auth[1];
-  
-    if (username == 'admin' && password == 'password'){
-      req.session.user = 'admin';
+  }
+  else {
+    if (req.session.user === 'authenticated') {
       next();
-    }else{
-      var err = new Error('You are not authenticated! ');
-      res.setHeader('www-Authenticate', 'Basic');
-      err.status = 401;
-      return next(err);
     }
-  }else{
-    if(req.session.user == 'admin'){
-      next();
-    }else{
-      var err = new Error('You are not authenticated! ');
-
-      err.status = 401;
+    else {
+      var err = new Error('You are not authenticated!');
+      err.status = 403;
       return next(err);
     }
   }
@@ -83,8 +68,7 @@ app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 app.use('/dishes', dishRouter);
 app.use('/leaders', leaderRouter);
 app.use('/promotions', promoRouter);
